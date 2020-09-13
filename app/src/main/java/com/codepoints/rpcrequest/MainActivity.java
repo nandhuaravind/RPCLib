3c7e34728da759ee35bc1b5327ae1a35e3ea96b4
nandhuaravind <52998930+nandhuaravind@users.noreply.github.com>
2020-09-13 13:34:55 +0530

    first commit

---
+++ app/src/main/java/com/codepoints/rpcrequest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.codepoints.rpcrequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nandhu.rpclib.api.RPCRequest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
